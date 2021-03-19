958099a9e141bfa6430fc8fd35c38cedbb14b49b
Vinicius Telles <icehower@Viniciuss-MacBook-Air.local>
2021-03-18 22:40:42 -0300

    Challenge Completed

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,22 @@
-export function Content() {
-  // Complete aqui
+
+
+interface ContentProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export function Content(props: ContentProps) {
+  return(
+  <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.children}
+          </div>
+        </main>
+  </div>
+  )
 }
